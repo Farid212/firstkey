d33acb3c62b892c2a5a1cbbcad8e8e2cb9d99f9e
Farid <farid.hammouten@gmail.con>
2019-09-30 10:56:41 +0900

    have to put bounce key effect and google text-to-speech api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-(()=>{})();
+(() => {
+  let msg = document.getElementById("msg");
+
+  document.addEventListener("keypress", event => changeActualKey(event));
+
+  const randomColor = () => {
+    let hex = Math.round(Math.random() * 0xffffff).toString(16);
+    while (hex.length < 6) hex = "0" + hex;
+    return hex;
+  };
+
+  const changeActualKey = param => {
+    document.getElementsByClassName("contentBorder")[0].style.background = "#" + randomColor();
+    
+    msg.innerHTML = param.key;
+    msg.className = zoomer;
+  };
+})();
