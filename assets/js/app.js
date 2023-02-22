f9fe9f08ca482e243028ff824348bec13d6de047
farid hammouten <farid.hammouten@gmail.com>
2023-02-22 19:35:21 +0900

    refactored

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,44 @@
-document.addEventListener("keypress", function (event) {
-  goInvisible();
-  console.log("event: ", event);
-  const verified = verify(event);
-  let actualKey = generateKeyCode();
-  console.log("generateKC: ", actualKey);
-  swapKey(actualKey);
+let score = 0;
+let gameStarted = false;
+const correctAnswer = new Audio("./audio/CA1.mp3");
+const wrongAnswer = new Audio("./audio/WA3.mp3");
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space" && !gameStarted) {
+    gameStarted = true;
+    updateGame();
+  } else if (gameStarted) {
+    const gameEl = document.getElementById("msg");
+    if (event.key === gameEl.innerHTML) {
+      score++;
+      playSound(true);
+      updateGame();
+    } else {
+      playSound(false);
+    }
+  }
+  const scoreEl = document.getElementById("score");
+  scoreEl.innerHTML = `Score: ${score}`;
 });
 
-function goInvisible() {
-  $(".contentBorder")[0].style.background = "#" + randomColor();
-  $("#msg").fadeOut();
+function playSound(bool) {
+  if (!bool) {
+    wrongAnswer.currentTime = 0;
+    wrongAnswer.play();
+  } else {
+    correctAnswer.currentTime = 0;
+    correctAnswer.play();
+  }
 }
 
-function randomColor() {
-  let hex = Math.round(Math.random() * 0xffffff).toString(16);
-  while (hex.length < 6) hex = "0" + hex;
-  return hex;
+function updateGame() {
+  const gameEl = document.getElementById("msg");
+  gameEl.innerHTML = generateRandomLetter();
 }
 
-async function swapKey(param) {
-  console.log(param);
-  await playSound(Boolean(param));
-  document.getElementById("msg").innerText = String.fromCharCode(param);
-  $("#msg")[0].style.color = "white";
-  $("#msg")[0].style.fontSize = "13rem";
-  $("#msg").fadeIn();
+// generate the game random letter
+function generateRandomLetter() {
+  const options = "abcdefghijklmnopqrstuvwxyz1234567890";
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
 }
-
-function generateKeyCode() {
-  return Math.floor(Math.random() * (122 - 97 + 1)) + 97;
-}
-
-function verify(keypressEvent) {
-  let answer = false;
-
-  const text = document.getElementById("msg").innerText;
-  const charCode = text.charCodeAt(0);
-
-  console.log("transformed CharCode: ", charCode);
-
-  return answer;
-}
-
-function playSound(e) {
-  const wrongAnswer = new Audio("./assets/audio/WA3.mp3");
-  const correctAnswer = new Audio("./assets/audio/CA1.mp3");
-
-  e
-    ? () => {
-        correctAnswer.currentTime = 0;
-        correctAnswer.play();
-      }
-    : () => {
-        wrongAnswer.currentTime = 0;
-        wrongAnswer.play();
-      };
-}
-
-function startGame() {}
