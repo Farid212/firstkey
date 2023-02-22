62b0a5dc53a889d81ebf0be86abdafd183eb8c27
farid hammouten <farid.hammouten@gmail.com>
2023-02-22 20:16:01 +0900

    refectored

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 let score = 0;
 let gameStarted = false;
-const correctAnswer = new Audio("./audio/CA1.mp3");
-const wrongAnswer = new Audio("./audio/WA3.mp3");
+const correctAnswer = document.getElementById("correctAnswer")
+const wrongAnswer = document.getElementById("wrongAnswer")
 
 document.addEventListener("keydown", (event) => {
   if (event.code === "Space" && !gameStarted) {
@@ -11,25 +11,17 @@ document.addEventListener("keydown", (event) => {
     const gameEl = document.getElementById("msg");
     if (event.key === gameEl.innerHTML) {
       score++;
-      playSound(true);
+      correctAnswer.currentTime = 0;
+      correctAnswer.play()
       updateGame();
     } else {
-      playSound(false);
+      wrongAnswer.currentTime = 0;
+      wrongAnswer.play()
     }
   }
   const scoreEl = document.getElementById("score");
   scoreEl.innerHTML = `Score: ${score}`;
 });
-
-function playSound(bool) {
-  if (!bool) {
-    wrongAnswer.currentTime = 0;
-    wrongAnswer.play();
-  } else {
-    correctAnswer.currentTime = 0;
-    correctAnswer.play();
-  }
-}
 
 function updateGame() {
   const gameEl = document.getElementById("msg");
