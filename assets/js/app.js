db24ad29aea99c5a08886000d6a0888647019cff
farid hammouten <farid.hammouten@gmail.com>
2023-02-23 13:38:40 +0900

    add random color

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 let score = 0;
 let gameStarted = false;
-const correctAnswer = document.getElementById("correctAnswer")
-const wrongAnswer = document.getElementById("wrongAnswer")
+const correctAnswer = document.getElementById("correctAnswer");
+const wrongAnswer = document.getElementById("wrongAnswer");
 
 document.addEventListener("keydown", (event) => {
   if (event.code === "Space" && !gameStarted) {
@@ -12,11 +12,14 @@ document.addEventListener("keydown", (event) => {
     if (event.key === gameEl.innerHTML) {
       score++;
       correctAnswer.currentTime = 0;
-      correctAnswer.play()
+      correctAnswer.play();
       updateGame();
+      document.getElementsByClassName(
+        "contentBorder"
+      )[0].style.backgroundColor = "#" + randomColor();
     } else {
       wrongAnswer.currentTime = 0;
-      wrongAnswer.play()
+      wrongAnswer.play();
     }
   }
   const scoreEl = document.getElementById("score");
@@ -26,6 +29,12 @@ document.addEventListener("keydown", (event) => {
 function updateGame() {
   const gameEl = document.getElementById("msg");
   gameEl.innerHTML = generateRandomLetter();
+}
+
+function randomColor() {
+  let hex = Math.round(Math.random() * 0xffffff).toString(16);
+  while (hex.length < 6) hex = "0" + hex;
+  return hex;
 }
 
 // generate the game random letter
